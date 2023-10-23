628799491ba8e6a5c01cf019687539cc1f5dab43
Kevin <kevbao24@gmail.com>
2023-10-23 20:59:47 +0000

    exercise completed

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,49 @@
+const width = 2;
+const height = 3;
+
+const area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+const bill = 3;
+const payment = 4;
+
+const change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+const quizzes = 5;
+const midterm = 10;
+const final = 20;
+
+const grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+const firstName = 'Kevin';
+const lastName = 'Bao';
+
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const pH = 9;
+
+const isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+const headCount = 10;
+const isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+let motto = fullName;
+
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
