37a9350e101e6eae0be44583c93d177b75c6bb97
Kevin <kevbao24@gmail.com>
2023-10-24 16:40:49 +0000

    data-model-revision

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,32 @@
+const orderHistory = [
+  {
+    product: 'JavaScript for impatient programmers',
+    price: 31.55,
+    ordered: 'Aug 4, 2020',
+    delivered: 'Aug 8, 2020',
+  },
+  {
+    product: 'The Timeless Way of Building',
+    price: 41.33,
+    ordered: 'Jul 19, 2020',
+    delievered: 'Jul 20, 2020',
+  },
+  {
+    product: 'Gamecube Controller Adapter',
+    price: 15.98,
+    ordered: 'Jul 4, 2020',
+    delivered: 'Jul 7, 2020',
+  },
+  {
+    product: 'GameCube Controller',
+    price: 94.95,
+    ordered: 'Jul 3, 2020',
+    delivered: 'Jul 5, 2020',
+  },
+  {
+    product: 'The Art of Sql',
+    price: 33.99,
+    ordered: 'Jul 3, 2020',
+    delivered: 'Jul 5, 2020',
+  },
+];
