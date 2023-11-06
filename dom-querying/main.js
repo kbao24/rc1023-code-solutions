ba1ad3e6656b495d0873868851bb974bb32723e4
Kevin <kevbao24@gmail.com>
2023-11-06 19:06:14 +0000

    consolelog

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,22 +1,22 @@
 console.log('hello, world');
 
 const element = document.querySelector('h1');
-console.log(element);
+console.log('h1', element);
 
 console.dir(element);
 
 const explanation = document.querySelector('#explanation');
 
-console.log(explanation);
+console.log('explanation', explanation);
 
 console.dir(explanation);
 
 const hint = document.querySelector('.hint');
-console.log(hint);
+console.log('hint', hint);
 console.dir(hint);
 
 const paragraph = document.querySelector('p');
-console.log(paragraph);
+console.log('p', paragraph);
 
 const example = document.querySelector('.example-link');
-console.log(example);
+console.log('example-link', example);
