57cf7f46747b103c216b8dda3cbd3ec95febbe4e
Kevin <kevbao24@gmail.com>
2023-12-04 18:31:36 +0000

    first commit

---
+++ objects-revisited/main.js
@@ -0,0 +1,31 @@
+/*
+const business = {
+  opens: "8:00AM",
+  closes: "5:00PM",
+  totalEmployees: 2,
+  daysOpen: ['M', 'W', 'F'],
+  employees: {
+    mike: {
+      position: "Manager",
+      daysOfWeekWorking: ['W', 'F']
+    },
+    candice: {
+      position: "Customer Service Associate",
+      daysOfWeekWorking: ['M', 'W']
+    }
+  }
+};
+
+function addWeekends(business) {
+  business.daysOpen = ['M', 'W', 'F', 'Sat', 'Sun']
+
+  for (const employee in business.employees) {
+    business.employees[employee].daysOfWeekWorking.push('Sat', 'Sun');
+  }
+  return business;
+}
+
+function addEmployees() {
+
+}
+*/
