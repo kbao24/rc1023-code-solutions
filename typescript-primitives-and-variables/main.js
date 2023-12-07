cd7809f6444508167f0b44108c8d994bd42d3da1
Kevin <kevbao24@gmail.com>
2023-12-07 00:23:19 +0000

    operators

---
+++ typescript-primitives-and-variables/main.js
@@ -0,0 +1,17 @@
+// console.log('hello');
+var fullName = 'John Doe';
+var isCool = true;
+var totalPets = 2;
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', unknown);
+// console.log('hello');
+var never = undefined;
+console.log('value of never:', never);
+console.log('typeof never:', never);
---
+++ typescript-primitives-and-variables/main.ts
@@ -0,0 +1,22 @@
+// console.log('hello');
+const fullName: string = 'John Doe';
+const isCool: boolean = true;
+const totalPets: number = 2;
+
+console.log('value of fullName:', fullName);
+console.log('value of isCool:', isCool);
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown: null = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', unknown);
+
+// console.log('hello');
+
+const never: undefined = undefined;
+console.log('value of never:', never);
+console.log('typeof never:', never);
