8f56d62aa9beb11107b4b75e2f82f12bb5794409
Kevin <kevbao24@gmail.com>
2023-12-07 16:22:15 +0000

    operators

---
+++ typescript-operators-and-expressions/main.js
@@ -0,0 +1,40 @@
+var width = 5;
+var height = 6;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+var bill = 10;
+var payment = 15;
+var change = payment - bill;
+/*
+console.log("value of bill:", bill);
+console.log("typeof bill:", typeof(bill));
+
+console.log("value of payment:", payment);
+console.log("typeof payment:", typeof(payment));
+*/
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+var quizzes = 90;
+var midterm = 85;
+var final = 100;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+var firstName = 'Kevin';
+var lastName = 'Bao';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+var pH = 8;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+var headCount = 300;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
